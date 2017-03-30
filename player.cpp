ea9e49df71282aa663adc6181d34812e4208e543
ern0 <ern0@linkbroker.hu>
2017-03-30 11:35:31 +0200

    feature: plays music

--- player.cpp
+++ player.cpp
@@ -3,14 +3,16 @@
 #include <SDL2/SDL_audio.h>
 
 
-const int FREQ = 44100;
-const int VOLUME_MUL = 30;
+const int FREQ = 8000;
+const int VOLUME_MUL = 50;
 
 
 	uint32_t audioSample = 0;
 	
 	void audioCallback(void* userdata,unsigned char* stream,int len);
 	uint32_t byteBeat(uint32_t sample);
+	uint32_t divnz(uint32_t a,uint32_t b);
+	uint32_t modnz(uint32_t a,uint32_t b);
 
 
 	int main(int argc,char* argv[]) {
@@ -36,11 +38,13 @@ const int VOLUME_MUL = 30;
 		}
 
 		SDL_PauseAudioDevice(dev,0);
-		fprintf(stderr,"beep... ");
-		SDL_Delay(400);
+		fprintf(stderr,"playing... ");
+		SDL_Delay(30000);
 		fprintf(stderr,"\n");
+		exit(0);
+		
+		// was:
 		SDL_CloseAudioDevice(dev);
-
 		return 0;
 	} // main()
 
@@ -63,8 +67,29 @@ const int VOLUME_MUL = 30;
 	} // audioCallback()
 
 
+	uint32_t divnz(uint32_t a,uint32_t b) {
+		if (b == 0) return 0;
+		return a / b;
+	} // divnz()
+
+
+	uint32_t modnz(uint32_t a,uint32_t b) {
+		if (b == 0) return 0;
+		return a % b;
+	} // modnz()
+
+
 	uint32_t byteBeat(uint32_t t) {
+ 
+		uint32_t h = t / 2;
 
+		uint32_t drum = divnz(3300,modnz(h,2000)) * 17;
 
-		return t;
+		uint32_t bassline = ( modnz(h,2000*4) < (2000*1.2) ? h*4 : 0 );
+
+		uint32_t tunfm = (h*4) | ( 4*h * (( divnz(t,2000*8) & 3) + 5) );
+		uint32_t tune = ( modnz(h,2000) > (2000*0.55) ? tunfm : 0 ); 
+
+		return drum | bassline | tune;
+
 	} // byteBeat()
